49cd40c852f84a1e4d3731f95351a8c8e185e4b4
ramaneswaran <ramannewdelhi44@gmail.com>
2020-06-27 16:23:40 +0530

    Integrate API client

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 import spacy
 import torch
+import random
 from .core.nlu.spacy import SpacyModel, get_spacy
 from .core.summarizer.textrank import TextRank
 from .core.language.gpt2 import Model, get_model
+from .genius.artist import Artist
+from .genius import Client, get_client
+
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config
 
 from fastapi import FastAPI, Depends 
@@ -19,8 +23,16 @@ def status():
     return {'status':"success", 'message':"Server up and running"}
 
 @app.get('/story')
-def get_story(request: StoryRequest, gpt2: Model = Depends(get_model),
-                nlp: SpacyModel = Depends(get_spacy)):
+def get_story(request: StoryRequest, client: Client = Depends(get_client)):
     
-    
-    return {'status':"success", 'message':"Some story", 'story':story}
+    name = request.name
+    artist = client.search_artist(name)
+
+    #Choose a random song
+    seed = random.randint(0, len(artist.songs)-1)
+    _id = artist.songs[seed][0]
+
+    text = client.get_referents(_id)
+
+
+    return {'status':"success", 'message':"Some story", 'story':text}
