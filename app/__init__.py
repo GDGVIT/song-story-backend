87c4e1123180a37a55f853f3f79ead32e1618e48
ramaneswaran <ramannewdelhi44@gmail.com>
2020-06-27 11:59:32 +0530

    Add dependency injections

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,26 @@
 import spacy
 import torch
-from .core.nlu.spacy import SpacyModel
+from .core.nlu.spacy import SpacyModel, get_spacy
 from .core.summarizer.textrank import TextRank
-from .core.language.gpt2 import GPT2
+from .core.language.gpt2 import Model, get_model
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config
 
+from fastapi import FastAPI, Depends 
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class StoryRequest(BaseModel):
+    name: str
+
+
+@app.get("/")
+def status():
+    return {'status':"success", 'message':"Server up and running"}
+
+@app.get('/story')
+def get_story(request: StoryRequest, model: Model = Depends(get_model),
+                nlp: SpacyModel = Depends(get_spacy)):
+    
+    
+    return {'status':"success", 'message':"Some story"}
