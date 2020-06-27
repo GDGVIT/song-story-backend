d716ef7e1b0c84ec39fc4df7cf6f61ad47a43a3b
ramaneswaran <ramannewdelhi44@gmail.com>
2020-06-27 16:57:41 +0530

    Update GPT2

--- app/core/language/gpt2.py
+++ app/core/language/gpt2.py
@@ -14,15 +14,27 @@ class Model:
     
         self.model = GPT2LMHeadModel.from_pretrained("gpt2", pad_token_id=self.tokenizer.eos_token_id)
     
-    def get_story(self, context, max_length):
+    def get_text(self, context, max_length=100):
         '''
-        This function generates story
+        This generates text
         '''
         encoded_input = self.tokenizer.encode(context, return_tensors='pt')
         output = self.model.generate(encoded_input,do_sample=True, max_length=max_length, top_k=50)
         gen_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
 
         return gen_text
+    
+    def story(self, prompt, max_length=200):
+        '''
+        This function puts together the story
+        '''
+        story = self.get_text(prompt, max_length)
+        
+
+        start = story.find('.')
+        end = story.rfind('.')
+
+        return story[start+1:end]
 
 model = Model()
 
