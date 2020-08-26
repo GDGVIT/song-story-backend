3b7d46db4dce01f3a4624b2373e14864d719df89
Raman <raman@pop-os.localdomain>
2020-08-26 17:04:08 +0530

    Add t5 config

---
+++ prompt-generation/config.py
@@ -0,0 +1,10 @@
+from transformers import T5Tokenizer
+
+BATCH_SIZE = 1
+EPOCHS = 5
+LEARNING_RATE = 3e-5
+MAX_INPUT_LEN = 50
+MAX_TARGET_LEN = 36
+Tokenizer = T5Tokenizer.from_pretrained('t5-base')
+
+model_path = './models/t5-prompt.pt'
