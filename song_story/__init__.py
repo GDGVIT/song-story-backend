b5d0b1924090cd687cc6adc031c382d4145df7c2
Raman <raman@pop-os.localdomain>
2020-08-30 16:24:36 +0530

    Update main init file

--- song_story/__init__.py
+++ song_story/__init__.py
@@ -1,3 +1,4 @@
 from .core.nlu.spacy import SpacyModel as SpacyModel
 from .core.summarizer.textrank import TextRank as TextRank
-from .core.language.gpt2 import Model as GPT2
+from .core.language.gpt2 import GPT2_Model as GPT2
+from .core.language.t5 import T5_Model as T5
