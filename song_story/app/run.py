ee62b5b5327ab1a8576cca652ae53d57093eb2bb
Raman <raman@pop-os.localdomain>
2020-08-30 17:22:51 +0530

    Rename run.py

---
+++ song_story/app/run.py
@@ -0,0 +1,110 @@
+import os
+import random
+import json
+from dotenv import load_dotenv
+from rake_nltk import Rake
+
+from flask import Flask
+from flask import request, jsonify
+
+import spacy
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+
+from song_story import SpacyModel
+from song_story import TextRank
+from song_story import GPT2, T5
+from song_story import GeniusClient
+
+load_dotenv()
+
+
+class App(Flask):
+    def __init__(self, name):
+        super(App, self).__init__(name)
+
+        # Load models
+        print("[INFO] Loading models")
+
+        # Loading GPT2
+        gpt2_tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        # add the EOS token as PAD token to avoid warnings
+        gpt2_model = GPT2LMHeadModel.from_pretrained(
+            "gpt2", pad_token_id=gpt2_tokenizer.eos_token_id
+        )
+
+        # Initializing T5 Model
+        # t5_model = T5ForConditionalGeneration.from_pretrained("t5-base")
+        # t5_tokenizer = T5Tokenizer.from_pretrained("t5-base")
+        # t5_state_path = "./models/final.pt"
+        # t5_model.load_state_dict(torch.load(t5_state_path))
+
+        # Initializing classes
+        print("[INFO] Initializing classes")
+        # self.spacy_model = SpacyModel(size="md")
+        self.gpt2 = GPT2(gpt2_tokenizer, gpt2_model)
+        # self.t5 = T5(t5_tokenizer, t5_model, 100)
+        self.r = Rake()
+
+        token = os.getenv("ACCESS_TOKEN")
+
+        print("[TOKEN] {}".format(token))
+        self.client = GeniusClient(token)
+
+
+app = App(__name__)
+
+
+@app.route("/status", methods=["GET"])
+def status():
+    return {"status": "success", "message": "Server up and running"}
+
+
+@app.route("/context", methods=["GET"])
+def get_context():
+
+    name = request.args.get("name")
+    print(name)
+    artist = app.client.search_artist(name)
+
+    # Choose a random song
+    seed = random.randint(0, len(artist.songs) - 1)
+
+    _id = artist.songs[seed][0]
+    title = artist.songs[seed][1]
+
+    context = app.client.get_referents(_id)
+
+    return jsonify(
+        {
+            "status": "success",
+            "message": "Some story",
+            "story": context,
+            "title": title,
+        }
+    )
+
+
+@app.route("/keywords", methods=["POST"])
+def get_keywords():
+    data = json.loads(request.data)
+    text = data["text"]
+    app.r.extract_keywords_from_text(text)
+    keys = app.r.get_ranked_phrases()
+    keys = [key.lower().strip() for key in keys]
+
+    return {"phrase": " ".join(keys)}
+
+
+@app.route("/story", methods=["POST"])
+def get_story():
+    data = json.loads(request.data)
+    prompt = data["prompt"]
+    story = app.gpt2.story_from_prompt(prompt, 500)
+    return {"story": story}
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
